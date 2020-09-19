abb8cab360f69ae6c16c47656694bcb3de9617d3
LucasLuzBr <lucasribeirodaluzbr@gmail.com>
2020-09-19 09:28:10 -0300

    primeiro commit

---
+++ src/test/java/br/com/codificando/CodificandoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.codificando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodificandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
