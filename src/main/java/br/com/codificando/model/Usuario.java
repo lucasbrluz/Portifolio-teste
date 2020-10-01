a711f57e0207768fedc19fdfd0ce115699845258
LucasLuzBr <lucasribeirodaluzbr@gmail.com>
2020-10-01 13:32:54 -0300

    model e repository do usuario

---
+++ src/main/java/br/com/codificando/model/Usuario.java
@@ -0,0 +1,79 @@
+package br.com.codificando.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Size;
+
+import org.springframework.lang.NonNull;
+
+@Entity(name = "usuario")
+public class Usuario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NonNull
+    @Size(max = 100)
+    private String nome;
+
+    @NonNull
+    @Size(max = 100)
+    private String login;
+
+    @NonNull
+    @Size(max = 100)
+    private String senha;
+
+    @NonNull
+    @Size(max = 100)
+    private String cpf;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario [cpf=" + cpf + ", id=" + id + ", login=" + login + ", nome=" + nome + "]";
+    }
+
+}
